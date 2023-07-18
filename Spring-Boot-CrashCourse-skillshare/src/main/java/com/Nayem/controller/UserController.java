bd277252b7689d366eb1a2fa95ec86376ed104a8
Nayem73 <nayem73@gmail.com>
2023-07-18 19:39:36 +0600

    This won't work? I need to put it in a Constructor? why is that though? Something with Core Java?

--- Spring-Boot-CrashCourse-skillshare/src/main/java/com/Nayem/controller/UserController.java
+++ Spring-Boot-CrashCourse-skillshare/src/main/java/com/Nayem/controller/UserController.java
@@ -4,12 +4,18 @@ import com.Nayem.model.UserModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class UserController {
     @GetMapping
     public String fnc() {
         return "url working here, Alhamdulillah";
     }
+
+    public Map<String, UserModel> map = new HashMap<>();
+    map.put("Nayem", new UserModel("Nayem", "Mehedi", 180122));
 
     @GetMapping("/getUser")
     public UserModel getUser() {
