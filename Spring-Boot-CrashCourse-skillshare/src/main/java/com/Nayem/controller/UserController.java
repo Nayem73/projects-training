9ec787eb7cec49b851627eb2dbbb9ec9bb9916aa
Nayem73 <nayem73@gmail.com>
2023-07-19 06:24:24 +0600

    Error when doing PostMapping (Error = Cannot construct instance of `com.Nayem.model.UserModel` (no Creators, like default constructor, exist)). To clear this error, we need empty constructor inside UserModel class. This is serialize/deserialize stuff of objects. see more commit message in the next commit when I create an empty constructor

--- Spring-Boot-CrashCourse-skillshare/src/main/java/com/Nayem/controller/UserController.java
+++ Spring-Boot-CrashCourse-skillshare/src/main/java/com/Nayem/controller/UserController.java
@@ -1,9 +1,7 @@
 package com.Nayem.controller;
 
 import com.Nayem.model.UserModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,5 +22,10 @@ public class UserController {
     @GetMapping("/getUser/{userName}")
     public UserModel getUser(@PathVariable String userName) {
         return map.get(userName);
+    }
+
+    @PostMapping("/addUser")
+    public void addUser(@RequestBody UserModel user) {
+        map.put(user.getFirstName(), user);
     }
 }
